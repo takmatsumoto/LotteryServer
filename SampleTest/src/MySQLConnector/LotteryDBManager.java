d26e1f4c65ee7568467d939382a16c6ca849d116
Liu Li Wei <github.takmatsumoto@gmail.com>
2015-10-31 10:59:05 +0800

    新增SQL table name define

--- SampleTest/src/MySQLConnector/LotteryDBManager.java
+++ SampleTest/src/MySQLConnector/LotteryDBManager.java
@@ -12,7 +12,7 @@ import java.sql.Statement;
  *
  * @version 0.0.1
  * 
- * 1.撈取資料庫
+ * 1.�����澈
  */
 
 public abstract class LotteryDBManager {
@@ -20,12 +20,21 @@ public abstract class LotteryDBManager {
 	static final private String DBAdministrator = "root";
 	static final private String DBRootPassword = "pm1101tak1226";
 	static final private String DB_NAME = "LotteryDB";
+	
+	static final private String DB_TABLENAME_LOTTERY_539 = "Lottery539";
+	static final private String DB_TABLENAME_LOTTERY_BIGFU = "LotteryBigFu";
+	static final private String DB_TABLENAME_LOTTERY_BIGLOTTERY = "LotteryBigLottery";
+	static final private String DB_TABLENAME_LOTTERY_POWER38 = "LotteryPower38";
+	
+	
+	
+	
 
 	public LotteryDBManager() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void createTable(String tableName) {
+	protected void createTable(String tableName) {
 		
 	}
 	
